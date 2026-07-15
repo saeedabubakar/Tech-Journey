8fe85445b6bf0f37067b1b9bc88058486d206584
saeedabubakar <abubakarsaeed1909@gmail.com>
2026-07-14 18:17:49 -0700

    Complete Day 11: Tuples and Sets

---
+++ Python/exercise_day11.py
@@ -0,0 +1,6 @@
+students = ["Ali", "Fatima", "Ali", "John", "Fatima", "Mary"]
+unique_students = set(students)
+print(f"Original List:")
+print(f"{students}")
+print(f"Unique Students:")
+print(f"{unique_students}")
